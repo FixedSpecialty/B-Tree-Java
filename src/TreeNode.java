a0c8acd8fe5eccdbac437ffcf6e6c263879127d3
FixedSpecialty <42878649+FixedSpecialty@users.noreply.github.com>
2019-11-06 20:04:17 -0700

    TreeNode Update, added BTree class

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,14 @@
 
 public class TreeNode {
 
+	private static int parent; //byte offset of parent node
+	private static int[] childPointer; //byte offset of child pointers
+	private static TreeObject[] treeObject; //array of tree objects
+	
+	/**
+	 * MetaData for TreeNode
+	 */
+	private static int n; //number of keys currently stored
+	private static boolean leaf; //true if no left or right child
+	private static int ownLocation; //finds location of Node for cache
 }
--- src/TreeObject.java
+++ src/TreeObject.java
@@ -1,4 +1,7 @@
 
 public class TreeObject {
+	
+	private static long DNAString;
+	private static int frequency; //initialized as 0
 
 }
