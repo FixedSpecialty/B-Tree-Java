edb6b4c7d30d4d7f176cdfc79dd597e99cbc83e8
FixedSpecialty <42878649+FixedSpecialty@users.noreply.github.com>
2019-11-20 18:17:17 -0700

    TreeNode.java using LinkedList

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,9 +1,10 @@
+import java.util.LinkedList;
 
 public class TreeNode {
 
 	private static int parent; //byte offset of parent node
-	private static int[] childPointer; //byte offset of child pointers
-	private static TreeObject[] treeObject; //array of tree objects
+	private static LinkedList<Integer> children; //array of tree objects
+	private static LinkedList<TreeObject> keys; //array of tree keys
 	
 	/**
 	 * MetaData for TreeNode
@@ -12,15 +13,30 @@ public class TreeNode {
 	private static boolean leaf; //true if no left or right child
 	private static int ownLocation; //finds location of Node for cache
 	
+	public TreeNode()
+	{
+		parent = 0;
+		n = 0;
+		keys = new LinkedList<TreeObject>();
+		children = new LinkedList<Integer>();
+		
+	}
 	public int getN()
 	{
 		return n;
 		
 	}
-	
 	public void setN(int keyCount)
 	{
 		n = keyCount;
+	}
+	public void setLocation(int location)
+	{
+		ownLocation = location;
+	}
+	public int getLocation()
+	{
+		return ownLocation;
 	}
 	
 	public int getParent()
@@ -31,5 +47,65 @@ public class TreeNode {
 	public void setParent(int setP)
 	{
 		parent = setP;
+	}
+	
+	//for adding first child
+	public void addChild(int x)
+	{
+		children.add(x);
+	}
+	
+	//add specific for adding in an already populated node
+	public void addChild(int x, int i)
+	{
+		children.add(i, x);
+	}
+	
+	//return the child at the specified index of node
+	public int getChild(int i)
+	{
+		return children.get(i);
+	}
+	
+	//removes the child at the specified index of node
+	public int removeChild(int i)
+	{
+		return children.remove(i);
+	}
+	
+	//boolean to check if node is a leaf or not
+	public boolean leaf()
+	{
+		return leaf;
+	}
+	
+	//sets the boolean leaf value
+	public void setLeaf(boolean leaf)
+	{
+		this.leaf = leaf;
+	}
+	
+	//adds a treeObject to the node 
+	public void addKey(TreeObject x)
+	{
+		keys.add(x);
+	}
+	
+	//adds a treeObject at specific index of node
+	public void addKey(TreeObject x, int i)
+	{
+		keys.add(i, x);
+	}
+	
+	//returns the index if the key 
+	public TreeObject getKey(int i)
+	{
+		return keys.get(i);
+	}
+	
+	//removes the key
+	public TreeObject removeKey(int i)
+	{
+		return keys.remove(i);
 	}
 }
